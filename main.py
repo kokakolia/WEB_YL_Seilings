704b89c9b89bf84fff6e26fcb57aa9f67a155207
Kolia <razunikolai@gmail.com>
2024-04-05 16:00:34 +0400

    a little changes

---
+++ main.py
@@ -0,0 +1,18 @@
+from flask import Flask
+
+
+app = Flask(__name__)
+
+
+@app.route('/')
+@app.route('/about')
+def about():
+    return open('index.html', encoding='utf8')
+
+
+def main():
+    app.run(host='127.0.0.1', port=8080)
+
+
+if __name__ == '__main__':
+    main()
