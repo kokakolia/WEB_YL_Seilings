27a353dc2270bb016e9b25c85421b09b28e123d7
Kolia <razunikolai@gmail.com>
2024-04-13 14:29:02 +0400

    review page, html, css and blocks

--- main.py
+++ main.py
@@ -1,12 +1,18 @@
-from flask import Flask
+from flask import Flask, render_template
 
-app = Flask(__name__)
+app = Flask(__name__, template_folder='static/templates')
 
 
 @app.route('/')
 @app.route('/about')
 def about():
     return open('static/templates/about.html', encoding='utf8')
+
+
+@app.route('/reviews')
+def reviews():
+    reviews = ['1', '2']
+    return render_template('reviews.html', encoding='utf8', reviews=reviews)
 
 
 def main():
