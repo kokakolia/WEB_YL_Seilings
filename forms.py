5cc3c4b2a599c3de51dfcc4f5ed98cecbdbe74fd
IlyaChS <chistyak06@gmail.com>
2024-04-25 10:52:02 +0400

    full register commit

---
+++ forms.py
@@ -0,0 +1,26 @@
+from flask_wtf import FlaskForm
+from wtforms import StringField, PasswordField, SubmitField, BooleanField, EmailField, DateField, SelectField
+from wtforms.validators import DataRequired
+
+
+class LoginForm(FlaskForm):
+    email = StringField('Почта', validators=[DataRequired()])
+    password = PasswordField('Пароль', validators=[DataRequired()])
+    remember_me = BooleanField('Запомнить меня')
+    submit = SubmitField('Войти')
+
+
+class RegisterForm(FlaskForm):
+    email = EmailField('Почта', validators=[DataRequired()])
+    surname = StringField('Фамилия', validators=[DataRequired()])
+    name = StringField('Имя', validators=[DataRequired()])
+    sex = SelectField('Пол', choices=['Мужской', 'Женский'])
+    b_day = DateField('Дата рождения', validators=[DataRequired()])
+    password = PasswordField('Пароль', validators=[DataRequired()])
+    password_again = PasswordField('Повторите пароль', validators=[DataRequired()])
+    submit = SubmitField('Войти')
+
+
+class VerifyForm(FlaskForm):
+    value = StringField('', validators=[DataRequired()])
+    submit = SubmitField('Отправить')
--- main.py
+++ main.py
@@ -1,13 +1,33 @@
 from flask import Flask, render_template, redirect, request
-from werkzeug.security import generate_password_hash
-import datetime
-from loginform import LoginForm
+from werkzeug.security import generate_password_hash, check_password_hash
+from flask_mail import Message, Mail
+from forms import LoginForm, RegisterForm, VerifyForm
 from data import db_session
 from data.users import User
+from flask_login import login_user, LoginManager
+from random import randint
 
 
 app = Flask(__name__, template_folder='static/templates')
 app.config['SECRET_KEY'] = 'yandexlyceum_secret_key'
+app.config['MAIL_SERVER'] = 'smtp.gmail.com'
+app.config['MAIL_PORT'] = 587
+app.config['MAIL_DEFAULT_SENDER'] = 'tlt.ceilings63@gmail.com'
+app.config['MAIL_USERNAME'] = 'tlt.ceilings63@gmail.com'
+app.config['MAIL_PASSWORD'] = 'yedk uijg enpw zjhg'
+app.config['MAIL_USE_TLS'] = True
+app.config['MAIL_USE_SSL'] = False
+login_manager = LoginManager()
+login_manager.init_app(app)
+mail = Mail()
+mail.init_app(app)
+with app.app_context():
+    message = Message(subject='Код для подтверждения регистрации')
+
+
+users_info = {
+
+}
 
 
 def main():
@@ -15,7 +35,34 @@ def main():
     app.run(port=5500, host='127.0.0.1')
 
 
-@app.route('/')
+@login_manager.user_loader
+def load_user(user_id):
+    db_sess = db_session.create_session()
+    return db_sess.query(User).get(user_id)
+
+
+def setup_user(data):
+    surname = data['surname']
+    name = data['name']
+    sex = data['sex']
+    b_day = data['b_day']
+    email = data['email']
+    password = data['pwd']
+    db_sess = db_session.create_session()
+    user = User(
+        surname=surname,
+        name=name,
+        sex=sex,
+        b_day_date=b_day,
+        email=email,
+        password_hash=generate_password_hash(password)
+    )
+    db_sess.add(user)
+    db_sess.commit()
+
+
+
+@app.route('/', methods=['GET', 'POST'])
 @app.route('/about')
 def about():
     return render_template('about.html', encoding='utf8')
@@ -50,26 +97,70 @@ def order():
 def login():
     form = LoginForm()
     if form.validate_on_submit():
-        return redirect('/main')
+        db_sess = db_session.create_session()
+        user = db_sess.query(User).filter(User.email == form.email.data).first()
+        if user and check_password_hash(user.password_hash, form.password.data):
+            login_user(user, remember=form.remember_me.data)
+            return redirect("/")
+        return render_template('login.html',
+                               message="Неправильный логин или пароль",
+                               form=form)
     return render_template('login.html', title='Авторизация', form=form)
 
 
 @app.route('/registration', methods=['GET', 'POST'])
-def register():
-    if request.method == 'GET':
-        return render_template('register.html')
-    elif request.method == 'POST':
-        user = User()
-        user.name = request.form['name']
-        user.surname = request.form['surname']
-        user.sex = request.form['sex']
-        user.b_day_date = datetime.datetime.strptime(request.form['b_day'], '%Y-%m-%d').date()
-        user.email = request.form['email']
-        user.password_hash = generate_password_hash(request.form['password'])
+def reqister():
+    form = RegisterForm()
+    if form.validate_on_submit():
+        if form.password.data != form.password_again.data:
+            return render_template('register.html', title='Регистрация',
+                                   form=form,
+                                   message="Пароли не совпадают")
         db_sess = db_session.create_session()
-        db_sess.add(user)
-        db_sess.commit()
-        return 'Ok'
+        if db_sess.query(User).filter(User.email == form.email.data).first():
+            return render_template('register.html', title='Регистрация',
+                                   form=form,
+                                   message="Такой пользователь уже есть")
+        message.recipients = [form.email.data]
+        print()
+        user_info = {}
+        user_info['surname'] = form.surname.data
+        user_info['name'] = form.name.data
+        user_info['sex'] = form.sex.data
+        user_info['b_day'] = form.b_day.data
+        user_info['email'] = form.email.data
+        user_info['pwd'] = generate_password_hash(form.password.data)
+        users_info[form.email.data] = user_info
+        res = redirect('/verification')
+        res.set_cookie(key='email', value=form.email.data, max_age=600)
+        return res
+    return render_template('register.html', title='Регистрация', form=form)
+
+
+@app.route('/verification', methods=['GET', 'POST'])
+def verify():
+    form = VerifyForm()
+    email = request.cookies.get('email')
+    if not form.is_submitted():
+        code = randint(11111, 99999)
+        users_info[email]['code'] = code
+        message.body = f'Ваш код: {code}'
+        mail.send(message)
+        print(4)
+    if form.validate_on_submit():
+        code = users_info[email]['code']
+        print(3)
+        print(form.value.data, code)
+        if int(form.value.data) == int(code):
+            print(1)
+            setup_user(users_info[email])
+            res = redirect('/')
+            res.delete_cookie(key='email')
+            return res
+        print(2)
+        return render_template('register.html', title='Регистрация', message='Неверный код подтверждения', form=RegisterForm())
+    return render_template('verification.html', title='Верификация', form=form)
+
 
 if __name__ == '__main__':
     main()
